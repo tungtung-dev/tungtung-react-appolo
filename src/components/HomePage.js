24beb9689aa6cab625edeb8c32ad298d8fe0d617
thanhtungdp <tungptkh@gmail.com>
2016-12-20 16:18:33 +0700

    initial app

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class HomePage extends Component {
+    render() {
+        return <div>
+            HomePage 2
+        </div>
+    }
+}
+HomePage.propTypes = {}
+
