7c383f8516f558dd2a34218f6f070ce80095fffc
thanhtungdp <tungptkh@gmail.com>
2016-12-22 17:43:51 +0700

    change to localhost

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-    API_GRAPHQL: 'http://tungtung-graphql.herokuapp.com/graphql',
-    API_GRAPHQL_SOCKET: 'ws://tungtung-graphql.herokuapp.com',
-    // API_GRAPHQL: 'http://localhost:8080/graphql',
-    // API_GRAPHQL_SOCKET: 'ws://localhost:8080'
+    API_GRAPHQL: 'http://localhost:8080/graphql',
+    API_GRAPHQL_SOCKET: 'ws://localhost:8080'
 }
